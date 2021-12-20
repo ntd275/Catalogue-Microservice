1984c72884bc402552af8ae5f16217fdd13accd1
ntd275 <ntd275@gmail.com>
2021-12-20 20:34:33 +0700

    fix

--- models/database.js
+++ models/database.js
@@ -1,7 +1,7 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host: "catalogue_db",
+        host: "catalogue-db",
         user: "catalogue_user",
         password: 'default_password',
         database: 'socksdb',
