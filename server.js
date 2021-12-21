12c07aea6aa711f15d1f8dbb85be86dea3323933
ntd275 <ntd275@gmail.com>
2021-12-21 22:06:26 +0700

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 
 // app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 app.use('/catalogue/images/', express.static('./public'));
 
 routes(app)
